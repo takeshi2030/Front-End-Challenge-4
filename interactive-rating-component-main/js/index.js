350c9a55c6ea9cf8833a0be1b4ec8cad5a5ccb4d
André Maemura <andremaemura@gmail.com>
2022-08-25 09:22:58 -0300

    ajustes js

--- interactive-rating-component-main/js/index.js
+++ interactive-rating-component-main/js/index.js
@@ -2,6 +2,9 @@ const mainCard = document.querySelector('.card-main')
 const thanksCard = document.querySelector('.card-thanks')
 const submitButton = document.getElementById("submit")
 const rateAgain = document.getElementById("rate-again")
+const rating = document.getElementById("rating")
+const rates = document.querySelectorAll(".btn")
+
 
 submitButton.addEventListener("click", () => {
     thanksCard.classList.remove("hidden")
@@ -12,3 +15,10 @@ rateAgain.addEventListener("click", () => {
     thanksCard.classList.add("hidden")
     mainCard.style.display = "block"
 })
+
+rates.forEach((rate) => {
+    rate.addEventListener("click", () => {
+        rating.innerHTML = rate.innerHTML
+    })
+})
+
